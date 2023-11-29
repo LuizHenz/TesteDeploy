80a1d20760f6daa79f6f7bb83eea50ac83af4b00
LuizHenz <robertgito@hotmail.com>
2023-11-29 16:26:35 -0300

    update

--- back-end-aula05/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ back-end-aula05/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = DemoApplicationTests.class)
 class DemoApplicationTests {
 
 	@Test
